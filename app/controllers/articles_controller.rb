a18fec557bb0df301af98826d5bebf2e6a890209
Yoshiteru Negishi <yoshiteru-negishi@locoguide.co.jp>
2024-11-15 18:25:55 +0900

    Use r/w splitting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
   def index
+    ApplicationRecord.connected_to(role: :writing) do
+      Article.new(title: "New Article #{Time.zone.now}", body: "body").save!
+    end
+
     @articles = Article.all
   end
 end
